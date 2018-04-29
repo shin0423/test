7bfedff90897c88f5dd1ab0abccebda67971e9c1
shin0423 <bandos0423@gmail.com>
2018-04-30 01:04:48 +0900

    本棚

---
+++ bookshelf/create_table_bookshelf.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bookshelf.books (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    title VARCHAR(255),
+    image_url VARCHAR(255),
+    status VARCHAR(10),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
